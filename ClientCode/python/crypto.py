e7f334e886654538cb69323cdc3110b1e3a76713
Elton Vivot <eltonvivot@gmail.com>
2023-06-20 15:03:47 -0300

    fixed

--- ClientCode/python/crypto.py
+++ ClientCode/python/crypto.py
@@ -1,8 +1,8 @@
 from cryptography.fernet import Fernet
 import json, const
 
-def encrypt(message: bytes, key: bytes = const.FERNET_KEY) -> bytes:
-    return Fernet(key).encrypt(message)
+def encrypt(message: str, key: bytes = const.FERNET_KEY) -> bytes:
+    return Fernet(key).encrypt(message.encode())
 
 def decrypt(token: bytes, key: bytes = const.FERNET_KEY) -> bytes:
     return Fernet(key).decrypt(token)
